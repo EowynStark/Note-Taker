bd4b671b0ea318cce033ae124fdb95884f937339
EowynStark <andwellm89@gmail.com>
2024-01-10 16:00:07 -0600

    adding notes page route

--- Develop/public/assets/server.js
+++ Develop/public/assets/server.js
@@ -10,7 +10,12 @@ app.use(express.json());
 // data storage for potential notes
 let notes = [];
 
-// landing page
+// landing page route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// notes page route
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
