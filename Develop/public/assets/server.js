c6b86743d7ab4e7248bcff012d30258f6cc530f7
EowynStark <andwellm89@gmail.com>
2024-01-10 15:56:57 -0600

    adding landing page route and empty note variable

--- Develop/public/assets/server.js
+++ Develop/public/assets/server.js
@@ -6,3 +6,11 @@ const PORT = 3001;
 
 // middleware to handle JSON data
 app.use(express.json());
+
+// data storage for potential notes
+let notes = [];
+
+// landing page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
