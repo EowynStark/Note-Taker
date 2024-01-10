b38ab850520438d4690eecaf83c2e50ef6234a51
EowynStark <andwellm89@gmail.com>
2024-01-10 16:59:14 -0600

    adding initial functionality for notes file

--- Develop/public/assets/notes.js
+++ Develop/public/assets/notes.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const saveNoteButton = document.querySelector('.save-note');
+    const newNoteButton = document.querySelector('.new-note');
+    const clearButton = document.querySelector('.clear-btn');
+    const noteTitleInput = document.querySelector('.note-title');
+    const noteTextarea = document.querySelector('.note-textarea');
+    const listGroup = document.getElementById('list-group');
+
+    saveNoteButton.addEventListener('click', saveNote);
+    newNoteButton.addEventListener('click', newNote);
+    clearButton.addEventListener('click', clearForm);
+
+    fetchAndDisplayNotes();
+})
+
 // fetching existing notess and updating the UI
 function fetchAndDisplayNotes() {
     fetch('/api/notes').then((response) => {
