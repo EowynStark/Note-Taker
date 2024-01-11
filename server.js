1b3443d9000c7bca2fd8c02dab7e50937b312bc5
EowynStark <andwellm89@gmail.com>
2024-01-11 12:50:03 -0600

    removed an unneeded line of code

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const { json } = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
