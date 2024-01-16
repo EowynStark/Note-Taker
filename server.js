7c94445b8c80de38d7a5389f2aad9032b46baa90
EowynStark <andwellm89@gmail.com>
2024-01-15 18:31:09 -0600

    adjusting file paths

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ const dbFilePath = path.join(__dirname, 'db', 'db.json');
 let notes = [];
 
 // landing page route
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('./', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop', 'public', 'index.html'));
 });
 
 // notes page route
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+    res.sendFile(path.join(__dirname, 'Develop', 'public', 'notes.html'));
 });
 
 // api routes
